fd558dbeb4ba55b4e7f458fb3f8a951f9d0a8ef8
Fitus <50241646+Fitus@users.noreply.github.com>
2019-09-20 13:40:13 +0200

    Documentation improved

--- Simple_Demo_step3.sh
+++ Simple_Demo_step3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "DISPLAYING STATISTICS FROM THE RUN OF ZALOHA"
+echo "DISPLAYING STATISTICS ON THE RUN OF ZALOHA"
 echo
-echo "Objects compared .................. $(awk 'END { print NR }' test_backup/.Zaloha_metadata/370_union_s_diff.csv)"
-echo "Removals from 'test_backup' ....... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/510_exec1.csv)"
-echo "Copies to 'test_backup' ........... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/520_exec2.csv)"
-echo "Objects in synchronized state ..... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/505_target.csv)"
+echo "Objects compared ........................... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/370_union_s_diff.csv)"
+echo "Unavoidable removals from 'test_backup' .... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/510_exec1.csv)"
+echo "Copies to 'test_backup' .................... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/520_exec2.csv)"
+echo "Reverse-copies to 'test_source' ............ $(awk 'END { print NR }' test_backup/.Zaloha_metadata/530_exec3.csv)"
+echo "Remaining removals from 'test_backup' ...... $(awk 'END { print NR }' test_backup/.Zaloha_metadata/540_exec4.csv)"
+echo "Objects in synchronized state .............. $(awk 'END { print NR }' test_backup/.Zaloha_metadata/505_target.csv)"
